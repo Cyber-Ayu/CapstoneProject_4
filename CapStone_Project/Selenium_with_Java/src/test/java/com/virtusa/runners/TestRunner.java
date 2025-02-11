9e409f46fdbdc10e273028b142ac72be21249e0b
Cyber-Ayu <ayushsonirbt@gmail.com>
2025-02-11 17:56:42 +0530

    selenium with java scenario updated

--- CapStone_Project/Selenium_with_Java/src/test/java/com/virtusa/runners/TestRunner.java
+++ CapStone_Project/Selenium_with_Java/src/test/java/com/virtusa/runners/TestRunner.java
@@ -4,6 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",
-        glue = "com.virtusa.stepDefinitions", plugin = {"pretty", "html:target/cucumber-reports.html"})
+        glue = "com.virtusa.stepDefinitions", plugin = {"pretty", "html:test-output/cucumber-reports.html"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
